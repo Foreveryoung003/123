67381b1725f3deaf0ee8a3fbb6c775075e073221
gk <gk@adamant.local>
2023-05-22 19:13:52 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+num = int(input('Введите число\n'))
+if num > 0 and num < 8:
+    if num == 1:
+        print('pn')
+    if num == 2:
+        print('vt')
+    if num == 3:
+        print('sr')
+    if num == 4:
+        print('cht')
+    if num == 5:
+        print('pt')
+    if num == 6:
+        print('sub')
+    if num == 7:
+        print('vosk')
+else:
+print('error')
